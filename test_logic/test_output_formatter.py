939d875e9a61f8a50c6be28909465a3f2d3c2681
geoneth <113865421+geoneth@users.noreply.github.com>
2026-09-02 20:05:17 -0700

    Update test_output_formatter.py

--- test_logic/test_output_formatter.py
+++ test_logic/test_output_formatter.py
@@ -3,4 +3,4 @@ sys.path.append("..")
 from logic import output_formatter as O
 
 def test_format_output():
-    assert O.format_output([[1,2,3],[4,5,6],[7,8,9]]) == [[3,6,9],[2,5,8],[1,4,7]]
+    assert O.format_output([[1,2,3],[4,5,6],[7,8,9]]) == [[7,8,9],[4,5,6],[1,2,3]]
